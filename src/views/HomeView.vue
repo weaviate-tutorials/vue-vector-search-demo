170d2b90f17c1c91a4153f3dacf2fdc3dda48f11
malgamves <malgamves@gmail.com>
2024-02-15 11:39:47 +0100

    adding import script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import weaviate, {
   type WeaviateClient,
-  ObjectsBatcher,
   ApiKey,
 } from "weaviate-ts-client";
 import { onMounted, ref } from "vue";
-import { useStorage } from "@vueuse/core";
 
 const results = ref();
 const step = ref(1);
 const searchQuery = ref("");
 const loading = ref(false);
-
-let stepState = useStorage("steps-local-storage", step.value);
+const collectionName = ref("RSSGoo");
 
 const client: WeaviateClient = weaviate.client({
   scheme: "https",
@@ -23,77 +20,13 @@ const client: WeaviateClient = weaviate.client({
   },
 });
 
-const classObj = {
-  class: "RSMusic",
-  vectorizer: "text2vec-openai",
-  moduleConfig: {
-    "text2vec-openai": {},
-    "generative-openai": {
-
-    },
-  },
-};
-
-async function addSchema() {
-  const res = await client.schema.classCreator().withClass(classObj).do();
-  
-}
-
-async function getJsonData() {
-  const file = await fetch(
-    "https://gist.githubusercontent.com/malgamves/ca7d28dae20672912c85c358d7e42e31/raw/c323dd7e1de78073d772df870b4056e1c3e52a1b/songs.json"
-  );
-  return file.json();
-}
-
-async function importMusic() {
-  // Get the questions directly from the URL
-  const data = await getJsonData();
-
-  // Prepare a batcher
-  let batcher: ObjectsBatcher = client.batch.objectsBatcher();
-  let counter = 0;
-  const batchSize = 100;
-
-  for (const songs of data) {
-    // Construct an object with a class and properties corresponding to data shape
-    const obj = {
-      class: "RSMusic",
-      properties: {
-        rank: songs.rank,
-        title: songs.title,
-        artist: songs.artist,
-        album: songs.album,
-        year: songs.year,
-      },
-    };
-
-    // add the object to the batch queue
-    batcher = batcher.withObject(obj);
-
-    // When the batch counter reaches batchSize, push the objects to Weaviate
-    if (counter++ == batchSize) {
-      // flush the batch queue
-      const res = await batcher.do();
-      console.log(res);
-
-      // restart the batch queue
-      counter = 0;
-      batcher = client.batch.objectsBatcher();
-    }
-  }
-
-  // Flush the remaining objects
-  const res = await batcher.do();
-  console.log(res);
-}
 
 async function nearTextQuery() {
   loading.value = true;
   results.value = null;
   const res = await client.graphql
     .get()
-    .withClassName("RSMusic")
+    .withClassName(collectionName.value)
     .withFields("title artist rank album")
     .withNearText({ concepts: [`"${searchQuery.value}"`] })
     // .withBm25({
@@ -102,7 +35,7 @@ async function nearTextQuery() {
     .withLimit(8)
     .do();
 
-  results.value = res.data.Get.RSMusic;
+  results.value = res.data.Get.RSSGoo;
   loading.value = false;
   return res;
 }
@@ -112,27 +45,22 @@ async function generativeSearchQuery() {
   results.value = null;
   const res = await client.graphql
     .get()
-    .withClassName("RSMusic")
+    .withClassName(collectionName.value)
     .withFields("title artist album rank year")
     .withNearText({ concepts: [`"${searchQuery.value}"`] })
     .withGenerate({
-      singlePrompt: "Give me a fun fact about the song {title} by {artist} in one short paragraph.",
+      singlePrompt: "write me a haiku about {title} by {artist}.",
     })
     .withLimit(5)
     .do();
 
-  results.value = res.data.Get.RSMusic;
+  results.value = res.data.Get.RSSGoo;
   loading.value = false;
   return res;
 }
 
 onMounted(async () => { });
 
-function nextStep() {
-  if (stepState != 3) {
-    stepState.value = stepState.value + 1;
-  }
-}
 </script>
 
 <template>
@@ -147,128 +75,29 @@ function nextStep() {
       Vector Search with Weaviate
     </h1>
 
-    <div class="items-center pb-20 pt-10 p-8">
+    <div class="items-center pb-6 pt-10 p-8">
       <h2 class="sr-only">Steps</h2>
 
-      <div>
-        <div class="overflow-hidden rounded-full bg-gray-200">
-          <div
-            class="h-2 rounded-full bg-lime-400"
-            :class="`${'w-' + stepState + '/3'}`"
-          ></div>
-        </div>
-
-        <ol class="mt-4 grid grid-cols-3 text-sm font-medium text-gray-500">
-          <li class="flex items-center justify-start text-black sm:gap-1.5">
-            <span class="hidden sm:inline"> 1. Initialize </span>
-          </li>
-
-          <li class="flex items-center justify-center sm:gap-1.5">
-            <span
-              class="hidden sm:inline"
-              :class="`${stepState >= 2 ? 'text-black' : ' text-gray-500'}`"
-            >
-              2. Import Data
-            </span>
-          </li>
-
-          <li class="flex items-center justify-end sm:gap-1.5">
-            <span
-              class="hidden sm:inline"
-              :class="`${stepState == 3 ? 'text-black' : ' text-gray-500'}`"
-            >
-              3. Search
-            </span>
-          </li>
-        </ol>
-      </div>
-
-      <div
-        class="items-center justify-center flex-col flex pt-6"
-        :class="`${stepState == 3 ? 'hidden' : ''}`"
-      >
-        <button
-          class="inline-block items-center rounded-full border border-teal-600 p-3 
-          bg-black text-lime-500 transition hover:border-black hover:bg-lime-500 hover:text-black focus:outline-none focus:ring focus:ring-yellow-400"
-          @click="nextStep()"
-        >
-          <span class="sr-only"> Next Step </span>
-
-          <svg
-            class="h-5 w-5 rtl:rotate-180"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M14 5l7 7m0 0l-7 7m7-7H3"
-            />
-          </svg>
-        </button>
-        <p class="text-xs pt-2 text-black">next step</p>
+      <div class="items-center flex justify-center">
+        <p class="text-center text-m">
+          Search the Rolling Stones 500 Top Songs of the last century with Keyword Search.
+        </p>
+        
       </div>
     </div>
 
-    <section v-if="stepState == 1">
-      <div class="mx-auto px-4 pt-6 sm:px-6 lg:pt-8">
-        <div class="p-6 md:pt-8 lg:pt-8">
-          <div class="mx-auto max-w-xl text-center">
-            <h2 class="text-2xl font-bold text-black md:text-3xl">
-              we've initialised weaviate for you
-            </h2>
-
-            <p class="hidden text-black sm:mt-4 sm:block">
-              all you have to do now is add a schema to your weaviate instance
-              by clicking the button below
-            </p>
-            <div class="mt-4 md:mt-8">
-              <button @click="addSchema()"
-          class="inline-block rounded border border-white bg-black px-12 py-3 text-sm font-medium text-lime-500 transition hover:border-black hover:bg-lime-500 hover:text-black focus:outline-none focus:ring focus:ring-yellow-400">
-          add schema
-        </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section v-if="stepState == 2">
-      <div class="mx-auto px-4 pt-6 sm:px-6 lg:pt-8">
-        <div class="p-6 md:pt-8 lg:pt-8">
-          <div class="mx-auto max-w-xl text-center">
-            <h2 class="text-2xl font-bold text-black md:text-3xl">
-              time to import some data
-            </h2>
-
-            <p class="hidden text-teal-black sm:mt-4 sm:block">
-              we're importing rolling stones top 500 songs of the last century,
-              once imported you can search it
-            </p>
-            <div class="mt-4 md:mt-8">
-              <button @click="importMusic()"
-          class="inline-block rounded border border-white bg-black px-12 py-3 text-sm font-medium text-lime-500 transition hover:border-black hover:bg-lime-500 hover:text-black focus:outline-none focus:ring focus:ring-yellow-400">
-          import data
-        </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <div v-if="stepState == 3" class="justify-center items-center">
+    <div class="justify-center items-center">
       <div class="flex items-center justify-center pt-3">
         <label for="Search" class="sr-only"> Search </label>
 
-        <input v-model="searchQuery" type="text" id="Search" placeholder="Search for..."
+        <input v-model="searchQuery" type="text" id="Search" placeholder="what are you looking for?"
           class="w-2/3 rounded-md border-gray-200 bg-slate-300 py-2.5 pl-4 pe-10 shadow-sm sm:text-sm placeholder:text-gray-800" />
-
       </div>
+      <p class="item-center flex justify-center text-xs text-gray-600 pt-2 pb-4">
+          try and search for "songs about love"
+        </p>
 
-      <div class="flex items-start justify-center pt-5 gap-2">
+      <div class="flex items-start justify-center pt-3 gap-2">
 
         <button @click="nearTextQuery()"
           class="inline-block rounded border border-white bg-black px-12 py-3 text-sm font-medium text-lime-500 transition hover:border-black hover:bg-lime-500 hover:text-black focus:outline-none focus:ring focus:ring-yellow-400">
